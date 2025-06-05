4b969443f8adf129d88bb9d3fa406e7e6e9e2cb5
Armin Mokri <arminmokri@gmail.com>
2025-06-05 19:34:41 -0400

    test

--- src/sliding_window/maximum_average_subarray_i/Main.py
+++ src/sliding_window/maximum_average_subarray_i/Main.py
@@ -38,22 +38,22 @@ class Test(unittest.TestCase):
         self.assertAlmostEqual(
             self.solution.findMaxAverage([1, 12, -5, -6, 50, 3], 4), 12.75
         )
-        self.assertAlmostEqual(self.solution.findMaxAverage([5], 1), 5)
+        self.assertEqual(self.solution.findMaxAverage([5], 1), 5)
 
     def test_all_positive(self):
-        self.assertAlmostEqual(self.solution.findMaxAverage([1, 2, 3, 4, 5], 2), 4.5)
+        self.assertEqual(self.solution.findMaxAverage([1, 2, 3, 4, 5], 2), 4.5)
 
     def test_all_negative(self):
-        self.assertAlmostEqual(self.solution.findMaxAverage([-1, -2, -3, -4], 2), -1.5)
+        self.assertEqual(self.solution.findMaxAverage([-1, -2, -3, -4], 2), -1.5)
 
     def test_single_element_window(self):
-        self.assertAlmostEqual(self.solution.findMaxAverage([5, 1, -2, 3], 1), 5.0)
+        self.assertEqual(self.solution.findMaxAverage([5, 1, -2, 3], 1), 5.0)
 
     def test_full_array_window(self):
-        self.assertAlmostEqual(self.solution.findMaxAverage([2, 4, 6, 8], 4), 5.0)
+        self.assertEqual(self.solution.findMaxAverage([2, 4, 6, 8], 4), 5.0)
 
     def test_large_numbers(self):
-        self.assertAlmostEqual(
+        self.assertEqual(
             self.solution.findMaxAverage([1000000, 1000000, 1000000], 2), 1000000.0
         )
 
