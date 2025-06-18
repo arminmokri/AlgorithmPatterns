80490c70e0a84f0f3317ea866e49a5686daaab46
Armin Mokri <arminmokri@gmail.com>
2025-06-18 12:12:52 -0400

    Coin Change - Count Ways To Make Sum

--- src/dynamic_programming/coin_change_count_ways/Main.py
+++ src/dynamic_programming/coin_change_count_ways/Main.py
@@ -1,55 +1,100 @@
+import unittest
+
 debug = True
 
 
-def listToString(myList: list | tuple) -> str:
-    items = [f"({i}) {item}" for i, item in enumerate(myList)]
-    return f"[{', '.join(items)}]"
+class Solution:
+    def martixToString(self, myMatrix: list[list] | tuple[tuple]) -> str:
+        if myMatrix == []:
+            return "[]"
+        elif myMatrix == [[]]:
+            return "[[]]"
 
+        str_matrix = [[str(val) for val in row] for row in myMatrix]
+        max_width = max(len(val) for row in str_matrix for val in row)
 
-def solution(N: int, coins: tuple) -> int:
-    ways = [0 for _ in range(N + 1)]
+        return "\n".join(
+            "[ " + ", ".join(f"{val:>{max_width}}" for val in row) + " ]"
+            for row in str_matrix
+        )
 
-    ways[0] = 1
+    def listToString(self, myList: list | tuple) -> str:
+        if myList == []:
+            return "[]"
 
-    # print
-    if debug:
-        print("coins=" + listToString(coins))
-        print()
+        items = [f"({i}) {item}" for i, item in enumerate(myList)]
+        return f"[{', '.join(items)}]"
 
-    for i, coin in enumerate(coins):
+    def coinChange(self, N: int, coins: tuple) -> int:
+        if debug:
+            print()
+
+        ways = [0 for _ in range(N + 1)]
+
+        ways[0] = 1
+
         # print
         if debug:
-            print("i=" + str(i) + " coin=" + str(coin))
+            print("coins=" + self.listToString(coins))
+            print()
 
-        for j in range(len(ways)):
-            if coin <= j:
-                # Update the ways array
-                index = j - coin
-                ways[j] = ways[j] + ways[index]
+        for i, coin in enumerate(coins):
+            # print
+            if debug:
+                print("i=" + str(i) + " coin=" + str(coin))
 
-                # print
-                if debug:
-                    print(
-                        "j="
-                        + str(j)
-                        + " index="
-                        + str(index)
-                        + " ways="
-                        + listToString(ways)
-                    )
+            for j in range(len(ways)):
+                if coin <= j:
+                    # Update the ways array
+                    index = j - coin
+                    ways[j] = ways[j] + ways[index]
 
-    # print
-    if debug:
-        print()
+                    # print
+                    if debug:
+                        print(
+                            "j="
+                            + str(j)
+                            + " index="
+                            + str(index)
+                            + " ways="
+                            + self.listToString(ways)
+                        )
 
-    return ways[N]
+        # print
+        if debug:
+            print()
+
+        return ways[N]
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_basic_case(self):
+        self.assertEqual(self.solution.coinChange(12, (1, 2, 5, 10)), 15)
+
+    def test_zero_amount(self):
+        self.assertEqual(self.solution.coinChange(0, (1, 2, 5)), 1)
+
+    def test_no_coins(self):
+        self.assertEqual(self.solution.coinChange(5, ()), 0)
+
+    def test_exact_single_coin_match(self):
+        self.assertEqual(self.solution.coinChange(5, (5,)), 1)
+
+    def test_coins_larger_than_target(self):
+        self.assertEqual(self.solution.coinChange(3, (4, 5)), 0)
+
+    def test_single_coin_multiple_ways(self):
+        self.assertEqual(self.solution.coinChange(4, (1, 2)), 3)
+
+    def test_large_target_small_coins(self):
+        self.assertEqual(self.solution.coinChange(10, (1,)), 1)
 
 
 def main():
-    N = 12
-    coins = (1, 2, 5, 10)
-    result = solution(N, coins)
-    print("Result=" + str(result))
+    unittest.main()
 
 
 if __name__ == "__main__":
