8e3b7b3f2c511c6e7dcaa5036ef5bfebc02583fc
Armin Mokri <arminmokri@gmail.com>
2026-01-17 09:08:08 -0500

    update PrintHelper

--- src/main/java/common/PrintHelper.java
+++ src/main/java/common/PrintHelper.java
@@ -1,5 +1,6 @@
 package common;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -47,5 +48,61 @@ public class PrintHelper {
                 .boxed()
                 .collect(Collectors.toList());
         return listToStringWithoutIndex(list);
+    }
+
+    public static String matrixToString(List<? extends List<?>> matrix) {
+        if (matrix.isEmpty()) {
+            return "[]";
+        }
+        if (matrix.size() == 1 && matrix.get(0).isEmpty()) {
+            return "[[]]";
+        }
+
+        // Convert all values to strings and find max width
+        int maxWidth = 0;
+        String[][] strMatrix = new String[matrix.size()][];
+
+        for (int i = 0; i < matrix.size(); i++) {
+            List<?> row = matrix.get(i);
+            strMatrix[i] = new String[row.size()];
+            for (int j = 0; j < row.size(); j++) {
+                String val = String.valueOf(row.get(j));
+                strMatrix[i][j] = val;
+                maxWidth = Math.max(maxWidth, val.length());
+            }
+        }
+
+        // Build formatted output
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strMatrix.length; i++) {
+            sb.append("[ ");
+            for (int j = 0; j < strMatrix[i].length; j++) {
+                sb.append(String.format("%" + maxWidth + "s", strMatrix[i][j]));
+                if (j < strMatrix[i].length - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append(" ]");
+            if (i < strMatrix.length - 1) {
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String matrixToString(Object[][] matrix) {
+        List<List<Object>> result = new ArrayList<>();
+
+        for (Object[] row : matrix) {
+            List<Object> listRow = new ArrayList<>();
+            if (row != null) {
+                for (Object val : row) {
+                    listRow.add(val);
+                }
+            }
+            result.add(listRow);
+        }
+        return matrixToString(result);
     }
 }
