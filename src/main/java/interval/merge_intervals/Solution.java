2c91ab4b20dded9067d4555bc6a681ecc9d84107
Armin Mokri <arminmokri@gmail.com>
2026-01-20 06:40:36 -0500

    update java merge_intervals

--- src/main/java/interval/merge_intervals/Solution.java
+++ src/main/java/interval/merge_intervals/Solution.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 public class Solution {
@@ -25,11 +26,12 @@ public class Solution {
         List<List<Integer>> newIntervals = new ArrayList<>();
 
         List<Integer> previousInterval = intervals.size() > 0 ? intervals.get(0) : null;
-        IntervalFunction hasOverlap = (a, b) -> a.get(1) >= b.get(0);
+        BiPredicate<List<Integer>, List<Integer>> hasOverlap =
+                (a, b) -> a.get(1) >= b.get(0);
 
         for (int i = 0; i < intervals.size(); i++) {
             List<Integer> interval = intervals.get(i);
-            if (hasOverlap.compare(previousInterval, interval)) {
+            if (hasOverlap.test(previousInterval, interval)) {
                 previousInterval = List.of(
                         previousInterval.get(0),
                         Math.max(previousInterval.get(1), interval.get(1))
@@ -46,8 +48,4 @@ public class Solution {
         return newIntervals;
     }
 
-    @FunctionalInterface
-    private interface IntervalFunction {
-        boolean compare(List<Integer> intervalA, List<Integer> intervalB);
-    }
 }
